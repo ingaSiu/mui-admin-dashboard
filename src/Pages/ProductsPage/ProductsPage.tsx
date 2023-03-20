ab9edac65c06878a144e912f9e737d949ec56d7f
ingaSiu <ingasiud@gmail.com>
2023-03-20 11:46:56 +0200

    grid layout testing

--- src/Pages/ProductsPage/ProductsPage.tsx
+++ src/Pages/ProductsPage/ProductsPage.tsx
@@ -1,7 +1,19 @@
-import React from 'react';
+import { Box, Grid } from '@mui/material';
+
+import ProductCard from './ProductCard';
 
 const ProductsPage = () => {
-  return <div>ProductsPage</div>;
+  return (
+    <>
+      <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 1, sm: 2, md: 4 }}>
+        {Array.from(Array(6)).map((_, index) => (
+          <Grid item xs={1} sm={2} md={4} key={index}>
+            <ProductCard />
+          </Grid>
+        ))}
+      </Grid>
+    </>
+  );
 };
 
 export default ProductsPage;
