53ab505d9434ef5277701fffb961ee7107555bc7
ingaSiu <ingasiud@gmail.com>
2023-03-26 19:40:55 +0300

    display cards in grid layout

--- src/Pages/ProductsPage/ProductsPage.tsx
+++ src/Pages/ProductsPage/ProductsPage.tsx
@@ -1,17 +1,20 @@
 import { Box, Grid } from '@mui/material';
 
 import ProductCard from './ProductCard';
+import products from '../../_mock/products';
 
 const ProductsPage = () => {
   return (
     <>
-      <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 1, sm: 2, md: 4 }}>
-        {Array.from(Array(6)).map((_, index) => (
-          <Grid item xs={1} sm={2} md={4} key={index}>
-            <ProductCard />
-          </Grid>
-        ))}
-      </Grid>
+      <Box sx={{ flexGrow: 1 }}>
+        <Grid container spacing={{ xs: 1, sm: 2, md: 3 }} columns={{ xs: 1, sm: 8, md: 12 }}>
+          {products.map((product) => (
+            <Grid item xs={2} sm={4} md={4} key={product.id}>
+              <ProductCard product={product}>xs=2</ProductCard>
+            </Grid>
+          ))}
+        </Grid>
+      </Box>
     </>
   );
 };
