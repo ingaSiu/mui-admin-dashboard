548042ba03a59ea7b260b99db93267d8141d5bc0
ingaSiu <ingasiud@gmail.com>
2023-03-29 10:37:54 +0300

    styled product card

--- src/Pages/ProductsPage/ProductCard.tsx
+++ src/Pages/ProductsPage/ProductCard.tsx
@@ -14,16 +14,20 @@ type product = {
 
 const ProductCard = ({ product }: product) => {
   return (
-    <Card key={product.id} sx={{ maxWidth: 345 }}>
+    <Card key={product.id} sx={{ width: 345 }}>
       <CardMedia sx={{ height: 250 }} image={product.cover} title={product.name} />
       <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          <Link>{product.name}</Link>
+        <Typography gutterBottom component="div">
+          <Link sx={{ textDecoration: 'none', color: 'black' }}>{product.name}</Link>
         </Typography>
         {product.priceSale ? (
           <Box component="span">
-            <Typography>${product.priceSale}</Typography>
-            <Typography variant="body2">${product.price}</Typography>
+            <Typography variant="subtitle2">
+              Sale: ${product.priceSale}
+              <Typography variant="subtitle2" component="span" sx={{ paddingLeft: 2 }}>
+                ${product.price}
+              </Typography>
+            </Typography>
           </Box>
         ) : (
           <Typography variant="body2">${product.price}</Typography>
